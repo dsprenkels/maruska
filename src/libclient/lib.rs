2bb99862ece3060c4010e81b879796551709ea8e
Daan Sprenkels <dsprenkels@gmail.com>
2016-05-04 17:59:37 +0200

    client: add tests for md5 function

--- src/libclient/lib.rs
+++ src/libclient/lib.rs
@@ -387,3 +387,20 @@ pub fn md5(p: &str) -> String {
     }
     c
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn md5() {
+        use super::md5;
+        assert_eq!(md5(""), "d41d8cd98f00b204e9800998ecf8427e");
+        assert_eq!(md5("a"), "0cc175b9c0f1b6a831c399e269772661");
+        assert_eq!(md5("abc"), "900150983cd24fb0d6963f7d28e17f72");
+        assert_eq!(md5("message digest"), "f96b697d7cb7938d525a2f31aaf161d0");
+        assert_eq!(md5("abcdefghijklmnopqrstuvwxyz"), "c3fcd3d76192e4007dfb496cca67e13b");
+        assert_eq!(md5("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"),
+                   "d174ab98d277d9f5a5611c2c9f419d9f");
+        assert_eq!(md5("12345678901234567890123456789012345678901234567890123456789012345678901234567890"),
+                   "57edf4a22be3c955ac49da2e2107b67a");
+    }
+}
